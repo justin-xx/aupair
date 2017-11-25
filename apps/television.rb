1b4a6518cffba45a29148a7b71ae613694752285
Justin Rich <justin@jar.local>
2017-11-25 01:39:26 -0500

    encapsulated more functionality and switched to ajax/json app

--- apps/television.rb
+++ apps/television.rb
@@ -1,95 +1,96 @@
 require 'sinatra'
+require 'json'
 require 'singleton'
 
 set :bind, '0.0.0.0'
 set :views, Proc.new { File.join(root, "../views") }
 
-CEC_COMMANDS = {
-   "on"  => "10:04",
-   "off" => "1F:36",
-   "set source" => "1F:82:XX:00"
-}
-
-SOURCE = {
-  "HDMI 1" => "10",
-  "HDMI 2" => "20",
-  "HDMI 3" => "30",
-  "HDMI 4" => "40",
-  "Smartcast" => "50"
-}
 
 class Television
+  
+  CEC_COMMANDS = {
+     "on"  => "10:04",
+     "off" => "1F:36",
+     "set_source" => "1F:82:XX:00"
+  }
+
+  SOURCE = {
+    "HDMI 1" => "10",
+    "HDMI 2" => "20",
+    "HDMI 3" => "30",
+    "HDMI 4" => "40",
+    "Smartcast" => "50"
+  }
 
   include Singleton
 
   def initialize(_address = 0)
     @address = 0
+    @status = 'off'
+    @active_source = nil
   end
 
   def turn_display_on
     cec_command('on')
+    @status = 'on'
   end
 
   def turn_display_off
     cec_command('off')
+    @status = 'off'
   end
 
-  def set_source(src='HDMI 3')
-    cec_command('set source', SOURCE[src])
+  def set_source(src='HDMI 1')
+    cec_command('on')
+    sleep 1
+    cec_command('set_source', SOURCE[src])
+    @active_source = src
+  end
+  
+  def to_json
+    {
+      address: @address,
+      status:  @status,
+      source:  @active_source,
+      result:  @result
+    }.to_json
   end
 
   private
 
   def cec_command(cmd = 'on', parameter = '')
+    @result = begin
+      execute_command(cmd, parameter)      
+    rescue Exception => e
+      "\n\n\n\nAn error has occurred\n\n#{e}\n\n#{e.backtrace}\n\n\n\n"
+    end
+  end
+  
+  def execute_command(cmd = 'on', parameter = '')
     `echo "tx #{CEC_COMMANDS[cmd].gsub("XX", parameter)}" | cec-client -s -d 0`
   end
 
 end
 
-
-get '/' do
+get '/tv' do
+  content_type :html
   @tv = Television.instance
   erb :television
 end
 
 
-get '/tv/off' do
+post '/tv' do
+  content_type :json
   @tv = Television.instance
-  @result = @tv.turn_display_off
-  erb :television
-end
-
-
-get '/tv/on' do
-  @tv = Television.instance
-  @result = @tv.turn_display_on
-  erb :television
-end
-
-
-get '/tv/hdmi1' do
-  @tv = Television.instance
-  @result = @tv.set_source('HDMI 1')
-  erb :television
-end
-
-
-get '/tv/hdmi2' do
-  @tv = Television.instance
-  @result = @tv.set_source('HDMI 2')
-  erb :television
-end
-
-
-get '/tv/hdmi3' do
-  @tv = Television.instance
-  @result = @tv.set_source('HDMI 3')
-  erb :television
-end
-
-
-get '/tv/hdmi4' do
-  @tv = Television.instance
-  @result = @tv.set_source('HDMI 4')
-  erb :television
+  
+  case params['action']
+  when 'set_source'
+    @tv.set_source(params['source'])
+  when 'turn_display_off'
+    @tv.turn_display_off
+  when 'turn_display_on'
+    @tv.turn_display_on
+  end
+  
+  @tv.to_json
 end
