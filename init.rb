1788aada1f253ac1acef8a8eefaa859443b56656
Justin Rich <justin@jar.local>
2017-11-30 20:29:57 -0500

    require gems once

--- init.rb
+++ init.rb
@@ -1,10 +1,3 @@
-require 'sinatra'
-require 'sinatra/base'
-require 'json'
-require 'singleton'
-require 'nest_thermostat'
-
-
 %w{
   sinatra sinatra/base singleton json nest_thermostat
 }.each {|gem| require gem }
