e1fc785b5cce0d2825354ef6cb0913c91f9fc664
Justin Rich <justin@jar.local>
2018-03-04 19:13:01 -0500

    ported nest_thermostat wrapper

--- models/thermostat.rb
+++ models/thermostat.rb
@@ -1,51 +1,54 @@
+require 'singleton'
+
 class Thermostat
   
   include Singleton
   
+  attr_reader :nest_api
+  
   def initialize()
-    @api_connection = set_api_connection
+    @nest_api = set_api_connection
   end
   
   def current_temperature
-    @api_connection.current_temperature
+    nest_api.current_temperature
   end
   
   def target_temperature
-    @api_connection.temperature
+    nest_api.temperature
   end
   
   def target_temperature_high
-    @api_connection.temperature_high
+    nest_api.temperature_high
   end
   
   def target_temperature_low
-    @api_connection.temperature_low
+    nest_api.temperature_low
   end
   
   def target_temperature_at
-    @api_connection.target_temperature_at
+    nest_api.target_temperature_at
   end
   
   def away
-    @api_connection.away
+    nest_api.away
   end
   
   def away=(_status=true)
-    @api_connection.away=_status
+    nest_api.away=_status
   end
   
   def humidity
-    @api_connection.humidity
+    nest_api.humidity
   end
   
+  def to_json
+    
+  end
   
   private
   
   def set_api_connection
-    begin
-      NestThermostat::Nest.new(email: 'nest@justinrich.com', password: '.Trseoms1972')
-    rescue Exception => e
-      nil
-    end    
+    NestThermostat::Nest.new(email: 'nest@justinrich.com', password: '.Trseoms1972')   
   end
 end
