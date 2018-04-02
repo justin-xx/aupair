42461ab1c65eeecd36dbf720cef62b248965e034
Justin Rich <justin@jar.local>
2018-04-01 21:48:25 -0400

    improved layout of header info

--- lib/infoboard/infoboard-five-day.py
+++ lib/infoboard/infoboard-five-day.py
@@ -22,7 +22,8 @@ def writePidFile():
     f.close()
 
 def font(size = 24, name = 'HelveticaNeue-01.ttf'):    
-    return ImageFont.truetype("/home/pi/Documents/aupair/current/lib/typefaces/{0}".format(name), size, encoding="utf-8");
+    #return ImageFont.truetype("/home/pi/Documents/aupair/current/lib/typefaces/{0}".format(name), size, encoding="utf-8");
+    return ImageFont.truetype("/Users/justin/Documents/Code/aupair/lib/typefaces/{0}".format(name), size, encoding="utf-8");
 
 def center_text(draw, msg, ):
     w, h = draw.textsize(msg)
@@ -49,8 +50,8 @@ icon_mappings = {
     'tstorms': 'CloudRainThunder'     
 }
 
-filename = '/home/pi/Documents/aupair/current/lib/infoboard/infoboard-five-day.png'
-writePidFile()
+fourday_filename = '/home/pi/Documents/aupair/current/lib/infoboard/infoboard-five-day.png'
+#writePidFile()
 
 while True:
     try:
@@ -59,37 +60,48 @@ while True:
         end
 
     current_conditions = r.json()['current_conditions']
-    
+    print(current_conditions)
     forecasts_by_day = r.json()['forecasts']['forecasts_by_day']
     print(forecasts_by_day)
     
-    img = Image.new('RGBA', (1920, 1080), (255, 0, 0, 0))
-
-    draw = ImageDraw.Draw(img)
+    fourday_img = Image.new('RGBA', (1920, 1080), (255, 0, 0, 0))
+    fourday_draw = ImageDraw.Draw(fourday_img)
 
     # draw.text((170,0), u"\u00b0", font=font(77),fill=(255, 255, 255, 255))
     
-    ## HEADER
-    draw.rectangle([(294.194,102.000), (294.194+464.531,102.000+90.113)],  (0, 0, 0, int(255*0.7)))
-
-    draw.rectangle([(758.725,102.000), (758.725 +237.447,102.000+90.113)],  (0, 0, 0, int(255*0.5)))
+    ##
+    ## FOUR DAY FORECAST
+    ##
     
-    draw.rectangle([(996.538,102.000), (996.538 +110.705,102.000+90.113)],  (0, 0, 0, int(255*0.3)))    
+    
+    ## HEADER BUCKETS
+    fourday_draw.rectangle([(294.194,102.000), (294.194+464.531,102.000+90.113)],  (0, 0, 0, int(255*0.5)))
 
-    draw.text((376.945,115.111), current_conditions['city'], font=font(55),fill=(255, 255, 255, 255))
+    fourday_draw.rectangle([(783.725,102.000), (783.725+237.447,102.000+90.113)],  (0, 0, 0, int(255*0.3)))
+
+    fourday_draw.rectangle([(1045.538,102.000), (1045.538+110.705,102.000+90.113)],  (0, 0, 0, int(255*0.3)))    
+
+    # CITY
+    fourday_draw.text((376.945,115.111), current_conditions['city'], font=font(55),fill=(255, 255, 255, 255))
 
     localTime = datetime.now().replace(tzinfo=pytz.utc).astimezone(pytz.timezone('America/New_York'))
-
-    draw.text((801.455,115.111), localTime.strftime('%I:%M'), font=font(55),fill=(255, 255, 255, 255))
     
-    draw.text((1013.286,115.111), str(int(current_conditions['temperature'])), font=font(55),fill=(255, 255, 255, 255))
-    draw.text((1075.286,115.111), u"\u00b0", font=font(55),fill=(255, 255, 255, 255))
+    # TIME
+    time_msg = localTime.strftime('%I:%M')
+    time_w, time_h = fourday_draw.textsize(time_msg, font=font(55))    
     
-    ## MAIN CONTAINER
-
-    # draw.rectangle([(294.194,218.696), (294.194+1329.612,218.696+690.263)],  (127, 127, 127, int(255*0.4)))
-
-    ## FORECAST BUCKETS
+    fourday_draw.text((783.725+((237.447-time_w)/2),115.111), time_msg, align='center',font=font(55),fill=(255, 255, 255, 255))    
+    
+    #fourday_draw.text((1124.286+((237.447-time_w)/2),115.111), u"\u00b0", font=font(55),fill=(255, 255, 255, 255))
+    
+    # TEMP
+    temp_msg = str(int(current_conditions['temperature']))
+    temp_w, temp_h = fourday_draw.textsize(temp_msg, font=font(55))    
+    fourday_draw.text((1045.538+((110.705-temp_w)/2),115.111), temp_msg, align='center',font=font(55),fill=(255, 255, 255, 255))
+    fourday_draw.text((1105.538+((110.705-temp_w)/2),115.111), u"\u00b0", font=font(55),fill=(255, 255, 255, 255))
+    
+    
+    ## FOUR DAY BUCKETS
     bucket_width  = 246.0008
     bucket_height = 690.263
     bucket_y_offset = 218.696
@@ -111,43 +123,48 @@ while True:
     conditions_of_week_y_offset = 736.091
     conditions_of_week = [forecasts_by_day[0]['conditions'], forecasts_by_day[1]['conditions'], forecasts_by_day[2]['conditions'], forecasts_by_day[3]['conditions']]
     
+    ## BUCKETS
     for x_offset in x_offsets:    
-        draw.rectangle([(x_offset,bucket_y_offset), (x_offset+bucket_width,bucket_y_offset+bucket_height)],  (26, 26, 26, int(255*0.7)))
+        fourday_draw.rectangle([(x_offset,bucket_y_offset), (x_offset+bucket_width,bucket_y_offset+bucket_height)],  (26, 26, 26, int(255*0.7)))
 
+    ## DAY OF WEEK
     for index, day in enumerate(days_of_week):
         fnt      = font(70)
         msg      = str(day).upper()
-        w, h     = draw.textsize(msg, font=fnt)
+        w, h     = fourday_draw.textsize(msg, font=fnt)
         x_offset = (bucket_width-w)/2 + x_offsets[index]
        
-        draw.text((x_offset, days_of_week_y_offset), msg, align='center',font=fnt,fill=(255, 255, 255, 255))
+        fourday_draw.text((x_offset, days_of_week_y_offset), msg, align='center',font=fnt,fill=(255, 255, 255, 255))
     
+    ## ICON
     for index, icon in enumerate(icons_of_week):
         #icon_filepath = '/home/pi/Documents/aupair/current/lib/infoboard/weather/{0}.png'.format(icon_mappings[icon])
-        icon_filepath = '/Users/justin/Documents/Code/aupair/lib/infoboard/weather/{0}.png'.format(icon_mappings[icon])        
+        icon_filepath = '/Users/justin/Documents/Code/aupair/lib/infoboard/weather/{0}.png'.format(icon_mappings[icon])
         icon_file = Image.open(icon_filepath)
-        
         x_offset = int((bucket_width-icon_file.width)/2 + x_offsets[index]-15)
                 
         # 3rd param indicates alpha mask to use for first parameter
-        img.paste(icon_file, (x_offset, int(icons_of_week_y_offset)), icon_file)
+        fourday_img.paste(icon_file, (x_offset, int(icons_of_week_y_offset)), icon_file)
     
+    ## HIGH TEMP
     for index, high in enumerate(highs_of_week):
         fnt      = font(117)
         msg      = high
-        w, h     = draw.textsize(msg, font=fnt)
+        w, h     = fourday_draw.textsize(msg, font=fnt)
         x_offset = (bucket_width-w)/2 + x_offsets[index]
        
-        draw.text((x_offset, highs_of_week_y_offset), msg, align='center',font=fnt,fill=(255, 255, 255, 255))
+        fourday_draw.text((x_offset, highs_of_week_y_offset), msg, align='center',font=fnt,fill=(255, 255, 255, 255))
     
+    ## LOW TEMP
     for index, low in enumerate(lows_of_week):
         fnt      = font(50)
         msg      = low
-        w, h     = draw.textsize(msg, font=fnt)
+        w, h     = fourday_draw.textsize(msg, font=fnt)
         x_offset = (bucket_width-w)/2 + x_offsets[index]
        
-        draw.text((x_offset, lows_of_week_y_offset), msg, align='center',font=fnt,fill=(255, 255, 255, 255))
-        
+        fourday_draw.text((x_offset, lows_of_week_y_offset), msg, align='center',font=fnt,fill=(255, 255, 255, 255))
+    
+    ## CONDITIONS    
     for index, conditions in enumerate(conditions_of_week):
         fnt      = font(46)
         words    = conditions.split()
@@ -160,13 +177,13 @@ while True:
                 parts.append(word) 
         
         msg      = "\n".join(parts)
-        w, h     = draw.textsize(msg, font=fnt)
+        w, h     = fourday_draw.textsize(msg, font=fnt)
         x_offset = (bucket_width-w)/2 + x_offsets[index]
        
-        draw.text((x_offset, conditions_of_week_y_offset), msg, align='center',font=fnt,fill=(255, 255, 255, 255))
+        fourday_draw.text((x_offset, conditions_of_week_y_offset), msg, align='center',font=fnt,fill=(255, 255, 255, 255))
 
-    
-    
-    img.save(filename, 'PNG')
+    fourday_img.show()
+    break
+    #fourday_img.save(filename, 'PNG')
 
-    time.sleep(60)
+    #time.sleep(60)
