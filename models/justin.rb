a914f8cc525c0ece28160316338d73ab33e52190
Justin Rich <justin@jar.attlocal.net>
2018-09-29 18:55:13 -0400

    perform automatic House actions based on Justin going away, or coming home

--- models/justin.rb
+++ models/justin.rb
@@ -5,7 +5,7 @@ class Justin
   
   include Singleton
   
-  attr_reader :location, :lat, :lng
+  attr_reader :home, :location, :lat, :lng
   
   def initialize
     @home = Geokit::LatLng.new(39.606932,-84.219543)
@@ -14,7 +14,17 @@ class Justin
 
   def update_location(_lat,_lng)
     puts "Updating Location\n#{_lat},#{_lng}"
-    @location = Geokit::LatLng.new(_lat,_lng)
+    
+    previously = away?
+    
+    @location = Geokit::LatLng.new(_lat,_lng)    
+  
+    _away = away?
+    
+    # If there is a change in at-home or away status after the new coordinates
+    if previously != _away
+      _away ? House.set_away : House.set_home
+    end
   end
   
   def away?
