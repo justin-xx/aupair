8a84cf0354692c17369341bc4c953c1781a826b5
Justin Rich <justin@jar.attlocal.net>
2018-10-11 23:05:28 -0400

    Track location changes in mongodb

--- models/justin.rb
+++ models/justin.rb
@@ -1,20 +1,30 @@
 require 'singleton'
 require 'json'
+require 'mongo'
 
 class Justin
   
   include Singleton
   
-  attr_reader :house, :location
+  attr_reader :house, :location, :locations
   
   def initialize
+    @db = Mongo::Client.new(
+      [ "#{AUPAIR_CONFIG["mongodb"]["ip"]}:#{AUPAIR_CONFIG["mongodb"]["port"]}" ], 
+      :database => AUPAIR_CONFIG["mongodb"]["database"] 
+    )
+    @db_locations = @db[:locations]
+    
     @house = House.instance
     update_location(@house.location.lat, @house.location.lng)
   end
 
   def update_location(_lat,_lng)    
-    # log all GPS locations a file for now
-    `echo #{Time.now.to_i}--#{_lat},#{_lng} >> /home/pi/Documents/aupair/shared/locations.txt`
+    @db_locations.insert_one({
+      time: Time.now.utc.to_i, 
+      lat: _lat, 
+      lng: _lng
+    })
     
     # If you're not updating the location, don't bother
     return nil if !@location.nil? && @location.lat == _lat && @location.lng == _lng
@@ -27,14 +37,20 @@ class Justin
     # Set @outside_geofence to nil, so next time instance function outside_geofence is called, the
     # distance from home will be re-calculated and cached
     @outside_geofence = nil
-    
-    puts "p: #{previously} n: #{outside_geofence}"
-    
+        
     # If there is a change in at-home or away status after the new coordinates
     if previously.nil? || (previously != outside_geofence)
       self.away=outside_geofence
     end
     puts
+  end
+  
+  def locations
+    @locations = []
+    @db_locations.find.each do |location|
+      @locations << location
+    end
+    @locations
   end
   
   def outside_geofence
