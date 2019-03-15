0c9d8998ab68304b97d3d026d9346006631922a9
Justin Rich <justin@jar.attlocal.net>
2019-03-14 22:53:49 -0400

    updated config for deployment

--- config/deploy/pi.rb
+++ config/deploy/pi.rb
@@ -39,7 +39,7 @@ namespace :deploy do
   
   task :setup_config do
     on roles(:web, :television) do
-      config = '{"features": {"weather": "true","nest": "true"},"weather": {"api": "eff657faed2487df","zip": "45342"},"hue": {"account":"LiE6iDTdrB8mtz-Ixi1Wvy6bJaS4CI4YLXbBCChw"},"nest": {"email": "nest@justinrich.com","password": ".Trseoms1972"},"mongodb": {"ip": "127.0.0.1","port":"27017","database": "aupair"},"google": {"maps": "AIzaSyCme2Y4zAOKbNfsESmta6B1niRKxBMLGMk"},"ignore": [{"lat": "39.59517841785846", "lng": "-84.23262744412749"},{"lat": "39.59591350223986", "lng":" -84.23185221734772"},{"lat": "39.610580", "lng": "-84.203350"},{"lat": "39.59198368253506", "lng": "-84.23263985867557"}]}'
+      config = '{"features": {"weather": "true","nest": "true"},"weather": {"api": "eff657faed2487df","zip": "45342"},"hue": {"account":"LiE6iDTdrB8mtz-Ixi1Wvy6bJaS4CI4YLXbBCChw"},"nest": {"email": "nest@justinrich.com","password": ".Trseoms1972"},"televisions": [{"ip": "192.168.0.58", "port": "3000"}],"mongodb": {"ip": "127.0.0.1","port":"27017","database": "aupair"},"google": {"maps": "AIzaSyCme2Y4zAOKbNfsESmta6B1niRKxBMLGMk"},"ignore": [{"lat": "39.59517841785846", "lng": "-84.23262744412749"},{"lat": "39.59428630746116", "lng": "-84.23107969980113"},{"lat": "39.59591350223986", "lng": "-84.23185221734772"},{"lat": "39.59500276600817", "lng": "-84.23038880420053"},{"lat": "39.610580","lng": "-84.203350"},{"lat": "39.59198368253506", "lng": "-84.23263985867557"}]}'
       execute "touch #{shared_path}/config.json"
       execute "echo '#{config}' > #{shared_path}/config.json"
     end
